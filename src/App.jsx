d7bf4c27600c828cb11503b557d1b8dd1761ceb7
DESKTOP-A0EDSUE\kauan <kauanmellocancelier2005@gmail.com>
2023-11-11 18:50:44 -0300

    Iniciado repositório git.

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Container } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const App = () => {
+  return (
+    <Container>
+        teste
+    </Container>
+  );
+}
+
+export default App;
